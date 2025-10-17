b4c6d3187dae407d08cec66174f09c5c9209b9c4
felxx <igorfelipemonteirocontato@gmail.com>
2025-10-17 20:24:33 -0300

    feat: cria o use case para a função de dellete de labels

---
+++ src/main/java/br/com/divulgaifback/modules/works/useCases/label/delete/DeleteLabelUseCase.java
@@ -0,0 +1,24 @@
+package br.com.divulgaifback.modules.works.useCases.label.delete;
+
+import br.com.divulgaifback.common.exceptions.custom.NotFoundException;
+import br.com.divulgaifback.modules.works.entities.Label;
+import br.com.divulgaifback.modules.works.repositories.LabelRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteLabelUseCase {
+
+    private final LabelRepository labelRepository;
+
+    @Transactional
+    @Secured("IS_ADMIN")
+    public void execute(Long id) {
+        Label label = labelRepository.findById(id.intValue())
+                .orElseThrow(() -> new NotFoundException("Label not found"));
+        labelRepository.delete(label);
+    }
+}
