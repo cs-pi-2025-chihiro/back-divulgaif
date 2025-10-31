3e47527ab33fdd2598b3b7a209875e84bd893015
Rakoski <mastrakoski@gmail.com>
2025-10-31 19:51:03 -0300

    feat: contagem de trabalhos parte 1

---
+++ src/main/java/br/com/divulgaifback/modules/works/useCases/work/getStats/GetStatsUseCase.java
@@ -0,0 +1,12 @@
+package br.com.divulgaifback.modules.works.useCases.work.getStats;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GetStatsUseCase {
+
+    public GetStatsResponse execute(GetStatsRequest request) {
+    }
+}
