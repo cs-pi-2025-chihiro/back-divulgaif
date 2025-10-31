c0a0a1d5a970a86a89791a9ec7f33cf4f3ad7e3f
Rakoski <mastrakoski@gmail.com>
2025-10-31 19:49:01 -0300

    feat: contagem de trabalhos correlatos

---
+++ src/main/java/br/com/divulgaifback/modules/works/useCases/work/getStats/GetStatsResponse.java
@@ -0,0 +1,9 @@
+package br.com.divulgaifback.modules.works.useCases.work.getStats;
+
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Setter
+public class GetStatsResponse {
+}
