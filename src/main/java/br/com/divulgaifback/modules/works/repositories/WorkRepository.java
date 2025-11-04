b2a62da8fe74cfa760b223320ede3841820f29d8
Rakoski <mastrakoski@gmail.com>
2025-11-04 15:32:22 -0300

    feat: repository do dashboard

--- src/main/java/br/com/divulgaifback/modules/works/repositories/WorkRepository.java
+++ src/main/java/br/com/divulgaifback/modules/works/repositories/WorkRepository.java
@@ -2,11 +2,54 @@ package br.com.divulgaifback.modules.works.repositories;
 
 import br.com.divulgaifback.common.repositories.BaseRepository;
 import br.com.divulgaifback.modules.works.entities.Work;
-import com.querydsl.core.types.Predicate;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface WorkRepository extends BaseRepository<Work, Integer> {
-    Page<Work> findAll(Predicate predicate, Pageable pageable);
+    long countAllByWorkStatusId(Integer workStatusId);
+
+    long countAllByLabelsId(Integer labelId);
+
+    long countAllByAuthorsId(Integer authorId);
+
+    @Query("SELECT COUNT(w) as total, s.name as name " +
+            "FROM Work w JOIN w.workStatus s " +
+            "GROUP BY s.id, s.name")
+    List<WorksByStatusProjection> getCountsByStatusGrouped();
+
+    interface WorksByStatusProjection {
+        Long getTotal();
+        String getName();
+    }
+
+    @Query("SELECT COUNT(w) as total, l.name as name " +
+            "FROM Work w " +
+            "JOIN w.labels l " +
+            "JOIN w.workStatus ws " +
+            "WHERE ws.name = :statusName " +
+            "GROUP BY l.id, l.name " +
+            "ORDER BY COUNT(w) DESC")
+    List<WorksByLabelProjection> getCountsByLabelGrouped(String statusName, Pageable pageable);
+
+    interface WorksByLabelProjection {
+        Long getTotal();
+        String getName();
+    }
+
+    @Query("SELECT COUNT(w) as total, a.name as name " +
+            "FROM Work w " +
+            "JOIN w.authors a " +
+            "JOIN w.workStatus ws " +
+            "WHERE ws.name = :statusName " +
+            "GROUP BY a.id, a.name " +
+            "ORDER BY COUNT(w) DESC")
+    List<WorksByAuthorProjection> getCountsByAuthorGrouped(String statusName, Pageable pageable);
+
+    interface WorksByAuthorProjection {
+        Long getTotal();
+        String getEmail();
+        String getName();
+    }
 }
